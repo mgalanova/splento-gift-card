12c591395f278d0fa54550a75e60cb485b79c2e8
Galanova Maria <g.mellony@gmail.com>
2016-09-04 21:20:20 +0300

    logs for document ready

--- js/splento.js
+++ js/splento.js
@@ -1,4 +1,8 @@
 $( document ).ready(function() {
+    console.log('document ready');
     var imgHeight=$(".background-img").height();
     $(".splento-margin").css({'margin-top':imgHeight/100*31+"px"});
 });
+
+console.log("script is here");
+console.log('Docuent state: ' + document.readyState);
